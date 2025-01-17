45c8ea83a3b3469fc51f90dca21cde1717ad1256
aadarshraj4321 <aadarshraj4321@gmail.com>
2025-01-17 16:26:09 +0530

    updated the address of website

--- app/components/common/footer.tsx
+++ app/components/common/footer.tsx
@@ -27,12 +27,13 @@ export default function Footer() {
                  <span className='text-white'>aadarshraj4321@gmail.com</span>
                
              </li>
+             <li> <span className='text-white'>asunny583@gmail.com</span></li>
              <li className="text-gray-400 flex items-center gap-2 text-sm">
                <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                </svg>
-               India, Asia, Earth
+               Prem Nagar, Sabour, Bhagalpur, Bihar, India
              </li>
            </ul>
          </div>
