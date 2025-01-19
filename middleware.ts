a9c453994d2cf180950942f52734bfef2666fdd8
aadarshraj4321 <aadarshraj4321@gmail.com>
2025-01-19 22:52:30 +0530

    adding the start tof the art model

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,29 @@
+// import { NextResponse } from 'next/server';
+// import type { NextRequest } from 'next/server';
+
+// export function middleware(request: NextRequest) {
+//   // Handle only the success path
+//   if (request.nextUrl.pathname === '/payment/success') {
+//     // Allow the page to load but modify headers to prevent caching
+//     const response = NextResponse.next();
+//     response.headers.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+//     response.headers.set('Pragma', 'no-cache');
+//     response.headers.set('Expires', '0');
+//     return response;
+//   }
+//   return NextResponse.next();
+// }
+
+// export const config = {
+//   matcher: '/payment/success',
+// };
+
+
+
+
+
+
+
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
@@ -17,7 +43,6 @@ export function middleware(request: NextRequest) {
 export const config = {
   matcher: '/payment/success',
 };
-
 
 
 
